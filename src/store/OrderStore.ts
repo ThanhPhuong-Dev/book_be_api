d72a32ee7c82d71577ad2c6e3967c3365e7ba4c7
ThanhPhuong-Dev <thanhphuong212002@gmail.com>
2024-05-27 10:32:30 +0700

    add createAt order

--- src/store/OrderStore.ts
+++ src/store/OrderStore.ts
@@ -7,6 +7,23 @@ export class OrderStore {
   }
 
   async buy(data: any) {
+    const now = new Date();
+    const test = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+      timeZone: "Asia/Ho_Chi_Minh",
+    };
+
+    const formatter = new Intl.DateTimeFormat("vi-VN", test as any);
+    const formattedDate = formatter.format(now);
+
+    data.createdAt = formattedDate;
+    data.updatedAt = formattedDate;
     return this.collection?.insertOne(data as Document);
   }
 
